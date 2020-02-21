d2e8ba6f31ed0d23bb5583f01a14befd7de5e6e7
JinnKaY <menendez91@live.com.ar>
2020-02-21 08:56:34 -0300

    Fifo aumentada a 64

--- FPGA_UART/UART_loop.vhd
+++ FPGA_UART/UART_loop.vhd
@@ -8,9 +8,11 @@ entity UART_loop is
 		rst_i: in std_logic;
 		uart_rxd_i: in std_logic;
 		uart_txd_o: out std_logic;
+		leds  : out std_logic_vector(2-1 downto 0);
 		rgb_1  : out std_logic_vector(3-1 downto 0);
 		rgb_2  : out std_logic_vector(3-1 downto 0);
 		empty_o: out std_logic;
+		switch : in std_logic;
 		full_o: out std_logic
 	);
 end;
@@ -19,7 +21,8 @@ architecture UART_loop_arq of UART_loop is
 
 	signal w_data_signal, r_dataSignal: std_logic_vector(7 downto 0);
 	signal rd_uart_signal, wr_uart_signal: std_logic;
-	signal emptySignal: std_logic;
+	signal emptySignal,switch_s: std_logic;
+	signal led_s : std_logic_vector(2-1 downto 0);
 	signal led_rgb_1 : std_logic_vector(3-1 downto 0);
 	signal led_rgb_2 : std_logic_vector(3-1 downto 0);
 	
@@ -37,7 +40,8 @@ architecture UART_loop_arq of UART_loop is
 		clk_i: in std_logic;
         rst_i: in std_logic;
 		r_data: in std_logic_vector(8-1 downto 0);
-		rx_empty : in std_logic;
+		switch : in std_logic;
+		leds : out std_logic_vector(2-1 downto 0);
 		led_rgb_1  : out std_logic_vector(3-1 downto 0);
 		led_rgb_2  : out std_logic_vector(3-1 downto 0);
 		w_data: out std_logic_vector(8-1 downto 0)
@@ -54,7 +58,7 @@ begin
 								-- DVSR = 100M / (16 * baud rate)
 								-- baud rate = 19200
 			DVSR_BIT	=> 9, 	-- # bits of DVSR
-			FIFO_W		=> 4 	-- # addr bits of FIFO
+			FIFO_W		=> 6 	-- # addr bits of FIFO
 								-- # words in FIFO=2^FIFO_W			
 		)
 		port map(
@@ -84,7 +88,8 @@ begin
 			clk_i 		=>  clk_i,
 			rst_i       =>  rst_i,
 			r_data      => r_dataSignal,
-			rx_empty    => emptySignal,
+			switch      => switch,
+			leds        => led_s,
 			led_rgb_1   => led_rgb_1,
 			led_rgb_2   => led_rgb_2,
 			w_data      => w_data_signal
@@ -94,7 +99,7 @@ begin
 	
 	rgb_1       <= led_rgb_1;
 	rgb_2       <= led_rgb_2;
-	
-	--w_data_signal <= std_logic_vector(unsigned(r_dataSignal) + 0);	
+	leds       <= led_s;
+
 end;
 	
